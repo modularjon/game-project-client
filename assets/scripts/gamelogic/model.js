17f098dae7f17616f13171f2125b0e770607ffdb
modularjon <modularjon@gmail.com>
2016-06-02 14:52:05 -0400

    Add new directory gamelogic and new model.js file
    Add new directory and file to store code for a model of the current user game.

---
+++ assets/scripts/gamelogic/model.js
@@ -0,0 +1,19 @@
+'use strict';
+
+let currentGame = {
+  "game": {
+    "id": null,
+    "cells": ["","","","","","","","",""],
+    "over":false,
+    "player_x": {
+      "id": null,
+      "email": ""
+      },
+    "player_o": {
+      "id": null,
+      "email": ""
+    }
+  }
+};
+
+module.exports = currentGame;
