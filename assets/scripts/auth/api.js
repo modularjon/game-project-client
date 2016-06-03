2b65b7311be36efba9a8e211e0da6e580def3126
modularjon <modularjon@gmail.com>
2016-06-03 10:54:24 -0400

    Remove gamelogic api functions from auth api.js

--- assets/scripts/auth/api.js
+++ assets/scripts/auth/api.js
@@ -39,31 +39,9 @@ const changePassword = (data) => {
   });
 };
 
-const indexGames = () => {
-  return $.ajax({
-    url: app.host + '/games/',
-    method: 'GET',
-    headers: {
-      Authorization: 'Token token=' + app.user.token,
-    },
-  });
-};
-
-const createGame = () => {
-  return $.ajax({
-    url: app.host + '/games/',
-    method: 'POST',
-    headers: {
-      Authorization: 'Token token=' + app.user.token,
-    },
-  });
-};
-
 module.exports = {
   signUp,
   signIn,
   signOut,
   changePassword,
-  indexGames,
-  createGame,
 };
