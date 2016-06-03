2c9030cd0c2e2a1218ebc37e2a6a7aa42c875e5f
modularjon <modularjon@gmail.com>
2016-06-03 10:05:26 -0400

    Comment out dead code

--- assets/scripts/gamelogic/logic.js
+++ assets/scripts/gamelogic/logic.js
@@ -1,5 +1,14 @@
 'use strict';
 
 const model = require('model');
-
-let playerToken = 'x';
+//
+// let playerToken = 'x';
+//
+// const getDivID = function(id) {
+//   return /\d/.exec($(id).attr('id'))[0];
+// };
+//
+// module.exports = {
+//   playerToken,
+//   getDivID,
+// };
