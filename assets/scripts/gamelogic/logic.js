34da510a9888b53a3af5669a59f92d343b7b7345
modularjon <modularjon@gmail.com>
2016-06-03 11:25:15 -0400

    Add gameOver to logic.js
    Add initial code for logic.js, using if statements to determine if x or o won,
    if there is a draw, or if to just exit the function and continue.

--- assets/scripts/gamelogic/logic.js
+++ assets/scripts/gamelogic/logic.js
@@ -1,6 +1,19 @@
 'use strict';
 
-
+const gameOver = function(board, count) {
+  let win = false;
+  let winner = '';
+  if (board cell possibilites is equal to ['x','x','x']) {
+    win = true;
+    winner = 'x';
+  } else if (board cell possibilites is equal to ['o','o','o']) {
+    win = true;
+    winner = 'o';
+  } else if (count === 9) {
+    win = true;
+    winner = 'draw';
+  }
+};
 // const model = require('model');
 //
 // let playerToken = 'x';
