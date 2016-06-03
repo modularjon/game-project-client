7a3d1520b3581da4de018205ac141ecd51908abf
modularjon <modularjon@gmail.com>
2016-06-03 10:38:51 -0400

    remove gamelogic handlers

--- assets/scripts/auth/events.js
+++ assets/scripts/auth/events.js
@@ -1,16 +1,8 @@
 'use strict';
 
 const getFormFields = require('../../../lib/get-form-fields');
-// const model = require('../gamelogic/model');
-// const logic = require('../gamelogic/model')
 const api = require('./api');
 const ui = require('./ui');
-
-let playerToken = 'x';
-
-let gameBoard = ['','','','','','','','',''];
-
-let playCount = 0;
 
 const onSignUp = function(event) {
   event.preventDefault();
@@ -43,72 +35,12 @@ const onChangePassword = function(event) {
     .fail(ui.failure);
 };
 
-const onGetIndexGames = function(event) {
-  event.preventDefault();
-  api.indexGames()
-    .done(ui.success)
-    .fail(ui.failure);
-};
-
-const onStartGame = function(event) {
-  event.preventDefault();
-  api.createGame()
-    .done(ui.success)
-    .fail(ui.failure);
-};
-
-const onPlayerMove = function(event) {
-  event.preventDefault();
-
-  let divID = /\d/.exec($(event.target).attr('id'));
-
-  if (gameBoard[divID] === '') {
-
-      gameBoard[divID] = playerToken;
-
-      $(event.target).text(gameBoard[divID]);
-
-      if (playerToken === 'x') {
-        playerToken = 'o';
-      } else {
-        playerToken = 'x';
-      }
-
-      playCount ++;
-
-      let data = {
-        game: {
-          cell: {
-            index: divID,
-            value: gameBoard[divID]
-          },
-          over: false
-        }
-      };
-      console.log(data);
-
-      api.updateGame(data)
-        .done(ui.playerMoveSuccess)
-        .fail(ui.failure);
-
-  } else {
-    console.log('uh oh');
-  }
-  //
-  // if (playCount === 9) {
-  //   future function that displays a draw;
-  // }
-
-};
 
 const addHandlers = () => {
   $('#sign-up').on('submit', onSignUp);
   $('#sign-in').on('submit', onSignIn);
   $('#sign-out').on('submit', onSignOut);
   $('#change-password').on('submit', onChangePassword);
-  $('#index-games').on('click', onGetIndexGames);
-  $('#new-game').on('click', onStartGame);
-  $('.col-xs-2').on('click', onPlayerMove);
 };
 
 module.exports = {
