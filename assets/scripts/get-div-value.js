573f5a79b2421b54abe6b89d7f0bc5daa15224cf
modularjon <modularjon@gmail.com>
2016-06-03 10:14:54 -0400

    Remove redundant js files
    Remove set-div-value and get-div-value

--- assets/scripts/get-div-value.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-const getDivValue = function(data) {
-  console.log(data.id);
-  console.log(data.firstChild.data);
-};
-
-module.exports = getDivValue;
--- assets/scripts/set-div-value.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const setDivValue = function(event) {
-  $(event).text('X');
-};
-
-module.exports = setDivValue;
