d3b5f59329c531f06dbbca16fed58daca1b1fa83
modularjon <modularjon@gmail.com>
2016-06-03 18:16:52 -0400

    Add or edit multiple global functions
    Add switchPlayer to toggle between x and o.
    
    Edit testColumns, testRows and testDiagonals to prevent endless loop hangup,
    and to prevent checking for wins when an empty string is present at a given
    game board index.
    
    Edit whoIsWinner to prevent returning anything other than a string with a value
    of x or o.

--- assets/scripts/gamelogic/logic.js
+++ assets/scripts/gamelogic/logic.js
@@ -2,41 +2,70 @@
 
 let playerToken = 'x';
 
-let gameBoard = ['x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x'];
+let gameBoard = ['', '', '', '', '', '', '', '', ''];
 
 let playCount = 0;
 
 let winner = '';
 
+let gameOver = false;
+
+const switchPlayer = function(player) {
+  if (player === 'x') {
+    return 'o';
+
+  } else {
+    return 'x';
+
+  }
+};
+
 const testColumns = function(board) {
 
   for (let i = 0; i < 3; i++) {
-    if (board[i] === board[i + 3] && board[i] === board[i + 6]) {
+
+    if (board[i] !== '' && board[i] === board[i + 3] && board[i] === board[i + 6]) {
       return board[i];
     }
   }
+
+  return;
 };
 
 const testRows = function(board) {
 
-  for (let i = 0; i < 7; i + 3) {
-    if (board[i] === board[i + 1] && board[i] === board[i + 2]) {
+  for (let i = 0; i < 7; i += 3) {
+    if (board[i] !== '' && board[i] === board[i + 1] && board[i] === board[i + 2]) {
       return board[i];
     }
   }
+
+  return;
 };
 
 const testDiagonals = function(board) {
 
-  if (board[0] === board[4] && board[0] === board[8]) {
+  if (board[0] !== '' && board[0] === board[4] && board[0] === board[8]) {
     return board[0];
 
-  } else if (board[2] === board[4] && board[2] === board[6]) {
+  } else if (board[2] !== '' && board[2] === board[4] && board[2] === board[6]) {
     return board[2];
+  }
+};
 
-  } else {
-    return;
+const whoIsWinner = function(board) {
 
+  if (testColumns(board) !== '' && testColumns(board) !== undefined) {
+    winner = testColumns(board);
+    return winner;
+
+  } else if (testRows(board) !== '' && testRows(board) !== undefined) {
+    winner = testRows(board);
+    return winner;
+
+  } else if (testDiagonals(board) !== '' && testDiagonals(board) !== undefined) {
+    winner = testDiagonals(board);
+    return winner;
   }
 };
 
@@ -45,7 +74,7 @@ module.exports = {
   gameBoard,
   playCount,
   winner,
-  testColumns,
-  testRows,
-  testDiagonals,
+  gameOver,
+  switchPlayer,
+  whoIsWinner,
 };
