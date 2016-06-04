6d0755e147fa65adfea622c34942a39e61fe395c
modularjon <modularjon@gmail.com>
2016-06-04 15:12:58 -0400

    Add shows and hides for sign out
    Add appropriate shows and hide for elements to be affected by sign out. Add text
    'Sign in to start!' to game message on a sign out. Fix signInSuccess message by
    changing game-message from id to class.

--- assets/scripts/auth/ui.js
+++ assets/scripts/auth/ui.js
@@ -15,6 +15,7 @@ const signInSuccess = function(data) {
   app.user = data.user;
   logic.currentUser = data.user;
 
+  console.log(logic.currentUser);
   if (logic.currentUser) {
     $('#sign-up').hide();
     $('#sign-in').hide();
@@ -24,14 +25,25 @@ const signInSuccess = function(data) {
     $('#index-games').show();
     $('#find-game').show();
     $('#signed-in').text('Signed in as: ' + logic.currentUser.email);
-    $('#game-message').text('Click to start! x goes first!');
+    $('.game-message').text('Click to start! x goes first!');
   }
 };
 
 const signOutSuccess = function() {
   app.user = null;
   logic.currentUser = null;
-  console.log(app);
+
+  if (!logic.currentUser) {
+    $('#sign-up').show();
+    $('#sign-in').show();
+    $('#sign-out').hide();
+    $('#change-password').hide();
+    $('#new-game').hide();
+    $('#index-games').hide();
+    $('#find-game').hide();
+    $('#signed-in').text('');
+    $('.game-message').text('Sign in to start!');
+  }
 };
 
 module.exports = {
